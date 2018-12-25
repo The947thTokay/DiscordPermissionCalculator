f2caa8433f130f2bc436294e704bbead9b265274
Mart Koster <the947thtokay@codingtime.nl>
2018-12-25 21:47:47 +0100

    Finish page and add JS to generate and display authorization link

--- main.js
+++ main.js
@@ -2,18 +2,45 @@ let inputs = document.getElementsByClassName('input')
 let permissionInputs = document.getElementsByClassName('permission')
 let adminBox = document.getElementById('administrator')
 
+let permissionInteger = 0
+const autorizationUrl = 'https://discordapp.com/api/oauth2/authorize?client_id={clientId}&scope=bot&permissions={permissions}'
 const update = function () {
   if (adminBox.checked === true) {
     for (let i = 0; i < permissionInputs.length; i++) {
       permissionInputs[i].setAttribute('disabled', 'disabled')
     }
+
+    permissionInteger = adminBox.value
+    displayResults()
   } else {
     for (let i = 0; i < permissionInputs.length; i++) {
       permissionInputs[i].removeAttribute('disabled')
     }
+
+    let checkedInputs = []
+    for (let i = 0; i < permissionInputs.length; i++) {
+      if (permissionInputs[i].checked === true) {
+        checkedInputs.push(permissionInputs[i])
+      }
+    }
+
+    permissionInteger = 0;
+    for (let i = 0; i < checkedInputs.length; i++) {
+      permissionInteger = permissionInteger | checkedInputs[i].value
+    }
+    displayResults()
   }
 }
 
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener('input', update, false)
 }
+
+function displayResults() {
+  document.getElementById('result').innerHTML = autorizationUrl
+    .replace('{clientId}', encodeURIComponent(document.getElementById('clientId').value))
+    .replace('{permissions}', String(permissionInteger))
+  document.getElementById('permissionsInteger').innerHTML = permissionInteger;
+}
+
+displayResults()
